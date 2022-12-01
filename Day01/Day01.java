defeba2ab647081afe41b141dbc96618c72fd3f6
robert-williamson1 <112511434+robert-williamson1@users.noreply.github.com>
2022-12-01 14:26:55 -0600

    part 1 done
    Sadge

--- Day01/Day01.java
+++ Day01/Day01.java
@@ -1,14 +1,75 @@
+import java.util.Scanner;
+import java.io.File;
+
 public class Day01 {
 
+    private final static String DATA_FILE = "sampleInput.txt";
+
     public static void part1() {
+        Scanner in;
+        int mostCalories = 0;
 
+        try {
+            in = new Scanner(new File(DATA_FILE));
+        } catch (Exception e) {
+            System.out.println(e);
+            return;
+        }
+
+        while (in.hasNext()) {
+            String line = in.nextLine();
+            int calories = 0;
+
+            while (!line.equals("") && in.hasNext()) {
+                calories += Integer.parseInt(line);
+                if (in.hasNext())
+                    line = in.nextLine();
+                else
+                    break;
+            }
+
+            if (calories > mostCalories) {
+                mostCalories = calories;
+            }
+
+        }
+
+        System.out.println(mostCalories);
     }
-
 
     public static void part2() {
-        
+        Scanner in;
+        int mostCalories = 0;
+
+        try {
+            in = new Scanner(new File(DATA_FILE));
+        } catch (Exception e) {
+            System.out.println(e);
+            return;
+        }
+
+        while (in.hasNext()) {
+            String line = in.nextLine();
+            int calories = 0;
+
+            while (!line.equals("") && in.hasNext()) {
+                calories += Integer.parseInt(line);
+                if (in.hasNext())
+                    line = in.nextLine();
+                else
+                    break;
+            }
+
+            if (calories > mostCalories) {
+                mostCalories = calories;
+            }
+
+        }
+        System.out.println(mostCalories);
     }
 
-
+    public static void main(String[] args) {
+        part2();
+    }
 
 }
