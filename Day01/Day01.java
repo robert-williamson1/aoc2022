7373ae8ecc155e4b384f2b26da04bb4da372c1e5
robert-williamson1 <112511434+robert-williamson1@users.noreply.github.com>
2022-12-02 13:54:35 -0600

    part 2 done

--- Day01/Day01.java
+++ Day01/Day01.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 import java.io.File;
+import java.util.ArrayList; 
+import java.util.Collections;
 
 public class Day01 {
 
-    private final static String DATA_FILE = "sampleInput.txt";
+    private final static String DATA_FILE = "input.txt";
 
     public static void part1() {
         Scanner in;
@@ -39,33 +41,31 @@ public class Day01 {
 
     public static void part2() {
         Scanner in;
-        int mostCalories = 0;
-
+        ArrayList<Integer> calorieList = new ArrayList<>(); 
         try {
             in = new Scanner(new File(DATA_FILE));
         } catch (Exception e) {
             System.out.println(e);
             return;
         }
-
         while (in.hasNext()) {
             String line = in.nextLine();
             int calories = 0;
 
             while (!line.equals("") && in.hasNext()) {
                 calories += Integer.parseInt(line);
-                if (in.hasNext())
-                    line = in.nextLine();
-                else
+                if(in.hasNext()){
+                line = in.nextLine();
+                }
+                else{
                     break;
+                }
             }
-
-            if (calories > mostCalories) {
-                mostCalories = calories;
-            }
-
+            calorieList.add(calories);
         }
-        System.out.println(mostCalories);
+        Collections.sort(calorieList);
+        Collections.reverse(calorieList);
+        System.out.println(calorieList.remove(0) + calorieList.remove(0) + calorieList.remove(0) );
     }
 
     public static void main(String[] args) {
